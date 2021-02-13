799b145d92c6805a3800d94ab33c15287c12fc20
Praveen Palanisamy <praveen.palanisamy@outlook.com>
2021-02-13 20:12:51 +0000

    Add minimal gym reg info

---
+++ src/playwrightgym/__init__.py
@@ -0,0 +1,22 @@
+#!/usr/bin/env python
+# PlaywrightGym Visual Web envs for RL Agent training
+__version__ = "0.0.1"
+
+import sys
+import os
+
+from gym.envs.registration import register
+
+sys.path.append(os.path.dirname(os.path.abspath(__file__)))
+
+
+_AVAILABLE_ENVS = {
+    "PlayrightLoginUserVisualEnv-v0": {
+        "entry_point": "playwrightgym.playwright_env:PlaywrightEnv",
+        "discription": "Login form with username, password",
+    },
+}
+
+
+for env_id, val in _AVAILABLE_ENVS.items():
+    register(id=env_id, entry_point=val.get("entry_point"))
