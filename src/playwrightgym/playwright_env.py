de9bab752dee1a82b14212be14dec4346da199a1
Praveen Palanisamy <praveen.palanisamy@outlook.com>
2021-02-13 08:39:53 +0000

    Use interactive playwright mode; TODO: Fix reward elem selector

--- src/playwrightgym/playwright_env.py
+++ src/playwrightgym/playwright_env.py
@@ -14,9 +14,10 @@ KEY_ACTION_MAP = {i: x for (i, x) in enumerate(list("" + string.ascii_uppercase 
 
 
 class PlaywrightEnv(gym.Env):
-    def __init__(self, env_config: typing.Dict):
+    def __init__(self, playwright, env_config: typing.Dict):
         self.env_config = env_config
         self.base_url = env_config.get("url", "http://localhost:8000/login-user.html")
+        self.playwright = playwright
         self.obs_im_shape = env_config.get(
             "obs_im_shape", {"width": 160, "height": 210}
         )
@@ -49,7 +50,7 @@ class PlaywrightEnv(gym.Env):
             dtype=np.float32,  # minval for tf.random.uniform for int is expected to be scalar :?;https://github.com/tensorflow/tensorflow/issues/39814
         )
         self.headless = self.env_config.get("headless", True)
-        self.playwright = sync_playwright()
+        # self.playwright = sync_playwright()
 
     def _get_screenshot(self) -> np.ndarray:
         screenshot_bytes = self.page.screenshot()
@@ -57,39 +58,40 @@ class PlaywrightEnv(gym.Env):
         return screenshot
 
     def reset(self) -> np.ndarray:
-        with self.playwright as p:
-            self.browser = p.chromium.launch(headless=self.headless)
-
-            self.page = self.browser.new_page()
-            self.page.set_viewport_size(self.obs_im_shape)
-            self.page.goto(self.base_url)
-            self.obs = self._get_screenshot()
-            self.step_num = 0
-            self.done = False
-            return self.obs
+        self.browser = self.playwright.chromium.launch(headless=self.headless)
+        self.page = self.browser.new_page()
+        self.page.set_viewport_size(self.obs_im_shape)
+        self.page.goto(self.base_url)
+        self.obs = self._get_screenshot()
+        self.step_num = 0
+        self.done = False
+        return self.obs
 
     def step(self, action):
         # action: [click_x, click_y, press_key]
         click_x, click_y, press_key_id = action
         press_key = KEY_ACTION_MAP.get(press_key_id, "")
-        with self.playwright:
-            if not self.done:
-                self.page.mouse.click(click_x, click_y)
+        if not self.done:
+            self.page.mouse.click(float(click_x), float(click_y))
+            if not press_key == "":  # Skip key
                 self.page.keyboard.press(press_key)
-                self.obs = self._get_screenshot()
-                self.reward = self.page.get(self.reward_elem_name, "value")
-                self.step_num += 1
-                if self.step_num >= self.max_step_num:
-                    self.done = True
-                self.info = {}
-                return self.obs, self.reward, self.done, self.info
-            else:
-                raise (ValueError("Episode done. Call reset()"))
+            self.obs = self._get_screenshot()
+            self.reward = self.page.get_attribute(
+                f"css=[id={self.reward_elem_name}]", "value"
+            )
+            self.step_num += 1
+            if self.step_num >= self.max_step_num:
+                self.done = True
+            self.info = {}
+            return self.obs, self.reward, self.done, self.info
+        else:
+            raise (ValueError("Episode done. Call reset()"))
 
 
 if __name__ == "__main__":
     env_config = {}
-    env = PlaywrightEnv(env_config=env_config)
+    playwright = sync_playwright().start()
+    env = PlaywrightEnv(playwright, env_config=env_config)
     done = False
     obs = env.reset()
     while not done:
@@ -99,3 +101,5 @@ if __name__ == "__main__":
             f"obs.shape:{next_obs.shape} \t Reward:{reward} \t Done:{done} \t Info:{info}"
         )
         input()
+    env.close()
+    playwright.stop()
